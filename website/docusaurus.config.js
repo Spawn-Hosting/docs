9678a0c0964f5829e4a237994cbefcb8c06956f7
Spawn-Hosting <socials@spawnhosting.com>
2021-11-15 17:17:42 +0400

    main

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Spawn-Hosting', // Usually your GitHub org/user name.
-  projectName: 'website-test', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   presets: [
     [
